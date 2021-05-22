afb16b08fe52a0e1dec931427ae179756ef0d7ef
abraham <agthankachan007@gmail.com>
2021-05-22 11:22:20 +0530

    first commit

---
+++ app/src/main/java/com/example/covidprediction/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.covidprediction
+
+import android.annotation.SuppressLint
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+//    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webViewSetup()
+    }
+
+//    @RequiresApi(Build.VERSION_CODES.O)
+//    @SuppressLint("SetJavaScriptEnabled")
+    private  fun webViewSetup() {
+        wb_webView.webViewClient = WebViewClient()
+        wb_webView.apply {
+            loadUrl("https://covidprediction.netlify.app/")
+            settings.javaScriptEnabled = true
+//            settings.safeBrowsingEnabled = true
+            settings.allowFileAccess = true
+        }
+
+    }
+    override  fun onBackPressed() {
+        if (wb_webView.canGoBack()) wb_webView.goBack() else super.onBackPressed()
+    }
+}
